8f424492c320facfb621d47f02c5a546a9e3fb31
jbobis1 <javibobis12@hotmail.com>
2022-03-30 10:01:13 +0200

    modificamos holamundo

--- javibobis/src/javibobis/Holamundo.java
+++ javibobis/src/javibobis/Holamundo.java
@@ -3,8 +3,9 @@ package javibobis;
 public class Holamundo {
 
 	public static void main(String[] args) {
-		System.out.println("PRUEBA Commit");
-		System.out.println("Alumno: Javier Bobis");
+		System.out.println("PRUEBA Commit ");
+		System.out.println("Alumno: Javier Bobis ");
+		System.out.println("Añada rama cambio1 ");
 	}
 
 }
