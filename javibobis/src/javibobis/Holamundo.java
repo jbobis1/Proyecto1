497a8c618c3d62cfab96c435ac8796d316a4ab47
jbobis1 <javibobis12@hotmail.com>
2022-03-30 09:14:37 +0200

    Parte dos del commit de prueba

--- javibobis/src/javibobis/Holamundo.java
+++ javibobis/src/javibobis/Holamundo.java
@@ -3,7 +3,8 @@ package javibobis;
 public class Holamundo {
 
 	public static void main(String[] args) {
-		System.out.println("SOLO PRUEBA");
+		System.out.println("PRUEBA Commit");
+		System.out.println("Alumno: Javier Bobis");
 	}
 
 }
